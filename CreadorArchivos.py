e47bc3ad30c52abc7483ff36f483a73bc3b33e3b
AinzWatch <eliezercr@gravitar.biz>
2023-06-11 16:29:32 -0600

    Nueva Version creador de archivos

---
+++ CreadorArchivos.py
@@ -0,0 +1,28 @@
+import os
+
+# Directorio donde se crearán los archivos
+directorio = "./archivos"
+
+# Verificar si el directorio existe, si no, crearlo
+if not os.path.exists(directorio):
+    os.makedirs(directorio)
+
+# Contenido del archivo
+contenido = """let a = 1+2
+//comentario
+fora(let i = 0; i < 5; i = i+1){
+    let b = 2
+    b = 1+i
+}"""
+
+string_lista = "["
+# Generar 100 archivos .txt
+for i in range(1, 101):
+    nombre_archivo = f"archivo{i}.txt"
+    string_lista += '"archivos/'+nombre_archivo+'" '
+    ruta_archivo = os.path.join(directorio, nombre_archivo)
+    
+    with open(ruta_archivo, "w") as archivo:
+        archivo.write(contenido)
+string_lista += "]"
+print(string_lista)
--- prueba.clj
+++ prueba.clj
@@ -1,9 +1,11 @@
-(defn verificar-parentesis [expresion]
-  (let [abiertos (count (re-seq #"\(" expresion))
-        cerrados (count (re-seq #"\)" expresion))]
-    (= abiertos cerrados)))
+(require '[clojure.java.io :as io])
 
-(def expresion "(1+1()")
-(if (verificar-parentesis expresion)
-  (println "Los paréntesis están balanceados.")
-  (println "Los paréntesis no están balanceados."))
+(defn read-txt-files [directory]
+  (let [files (file-seq (io/file directory))
+        txt-files (filter #(= (.getName %) ".txt") files)]
+    (map #(.getName %) txt-files)))
+
+(def txt-file-names (read-txt-files "C:/Users/eliez/OneDrive/Desktop/Clases/Clojure/archivos"))
+
+;; Imprime la lista de nombres de los archivos .txt
+(println txt-file-names)
